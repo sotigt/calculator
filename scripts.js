daee83606f8ac58cc453bf4e7e5742c55f4af2c2
sotigt <78357279+sotigt@users.noreply.github.com>
2024-03-15 22:40:31 +0100

    create functionality

--- scripts.js
+++ scripts.js
@@ -1,34 +1,96 @@
 let sum = 0;
+let addedNumbers = [];
+let currentOperator; 
 
 const display = document.querySelector('.display');
-const clear = document.querySelector('#clear');
-
-clear.addEventListener('click', () => {
-    sum = 0;
-    return display.innerText = sum;
-})
-
 display.innerText = sum;
 
-function add (... numArray) {
+/*---- Operator buttons ----*/
+let operatorBtns = Array.from(document.querySelectorAll('.operatorButton'));
+operatorBtns.map((op) => {
+    op.addEventListener('click', () => {
+        if(currentOperator) removeClass();
 
-    numArray.map((number) => {
-        sum += number
-    })
-    
-    console.log(sum);
+        op.classList.add("operatorButtonActive");
+        currentOperator = op.id;
+    });
+});
+
+/*---- Number buttons ----*/
+let numBtns = document.querySelectorAll('.numButton');
+Array.from(numBtns).map((number) => {
+    number.addEventListener('click', () => {
+        if(currentOperator) {
+            removeClass();  
+        }   
+
+        addedNumbers.push(Number(number.id));
+        
+        display.innerText = number.id;
+    });
+});
+
+
+const sumBtn = document.querySelector('#sum');
+sumBtn.addEventListener('click', () => operate());
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    sum = 0;
+    addedNumbers = [];
+    currentOperator;
+
+    if(currentOperator) removeClass();
+
     return display.innerText = sum;
+});
+
+
+function add () {
+    return display.innerText = sum = addedNumbers.reduce((total, currentNumber) => total + currentNumber);
 }
 
 function subtract () {
-
+    return display.innerText = sum = addedNumbers.reduce((total, currentNumber) => total - currentNumber);
 }
 
 function multiply () {
-
+    return display.innerText = sum = addedNumbers.reduce((total, currentNumber) => total * currentNumber);
 }
 
 function divide () {
+    return display.innerText = sum = addedNumbers.reduce((total, currentNumber) => total / currentNumber);
+}
+
+function removeClass () {
+    operatorBtns.map((operator) => {
+        operator.classList.remove("operatorButtonActive");
+    })
+}
+
+function operate () {
+    if(!currentOperator) return display.innerText = sum;
+
+    switch(currentOperator) {
+        case "add":
+            add()
+            break;
+        case "subtract":
+            subtract()
+            break;
+        case "multiply":
+            multiply()
+            break;
+        case "divide":
+            divide()
+            break; 
+    }
+
+    if(currentOperator) removeClass();
+
+    sum = 0;
+    addedNumbers = [];
+    currentOperator;
 
 }
 
