da79b02b00091abb2d7f6d7d42496cc813649ff1
sotigt <78357279+sotigt@users.noreply.github.com>
2024-03-15 16:39:11 +0100

    Create add and clear sum functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
+let sum = 0;
+
 const display = document.querySelector('.display');
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', () => {
+    sum = 0;
+    return display.innerText = sum;
+})
+
+display.innerText = sum;
+
+function add (... numArray) {
+
+    numArray.map((number) => {
+        sum += number
+    })
+    
+    console.log(sum);
+    return display.innerText = sum;
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
+
+
+
+
